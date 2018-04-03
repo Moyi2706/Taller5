f36be9e935a69574964dc4609fef77ac003a433f
Brandon Moyano <brandon_moyano@hotmail.com>
2018-04-03 10:42:10 -0300

    Creacion de archivos
    Creacion de archivos_1

---
+++ app/src/main/java/com/pits/b3co/pits/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pits.b3co.pits.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pits.b3co.pits.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
